2241e64ea068e6113a111c51b93925b1a5168673
SooJuWen <soojw-wp19@student.tarc.edu.my>
2022-09-20 12:30:36 +0800

    fix 19

--- EmpApp.py
+++ EmpApp.py
@@ -105,7 +105,11 @@ def GetEmpName():
         cursor2.execute(get_ln_sql)
 
         first_name = cursor1.fetchone()
-        last_name = cursor2.fetchone()    
+        last_name = cursor2.fetchone()
+
+        first_name = first_name.replace('(', '')
+        first_name = first_name.replace(',', '')
+        first_name = first_name.replace(')', '')
 
     return render_template('EditPayroll.html', name=first_name)
     #open("EditPayroll.html").read().format(name=first_name)
